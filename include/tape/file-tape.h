e23b314d07bc73895dd273f56b01082acb966282
Kirill Kotlyar <kirill.kotlyar17@gmail.com>
2026-05-10 23:36:07 +0300

    dev: declaration of FileTape added

--- include/tape/file-tape.h
+++ include/tape/file-tape.h
@@ -1,8 +1,32 @@
-//
-// Created by kirill on 5/10/26.
-//
+#pragma once
 
-#ifndef YADRO_TEST_TATLIN_FILE_TAPE_H
-#define YADRO_TEST_TATLIN_FILE_TAPE_H
+#include "tape.h"
 
-#endif //YADRO_TEST_TATLIN_FILE_TAPE_H
+#include <fstream>
+
+namespace tape {
+
+class FileTape : public ITape {
+  public:
+    explicit FileTape(const std::string& file);
+
+    std::int32_t read() const override;
+
+    void write(std::int32_t value) override;
+
+    void move_left() override;
+
+    void move_right() override;
+
+    bool is_begin() const noexcept override;
+
+    bool is_end() const noexcept override;
+
+    ~FileTape() override;
+
+  private:
+    std::size_t offset_ = 0;
+    std::fstream file_;
+};
+
+} // namespace tape
