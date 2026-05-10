3c22115755d604a714802b559b13b16ce1e871bf
Kirill Kotlyar <kirill.kotlyar17@gmail.com>
2026-05-10 23:35:49 +0300

    dev: is_begin, is_end methods added to ITape

--- include/tape/tape.h
+++ include/tape/tape.h
@@ -1,22 +1,22 @@
 #pragma once
+
 #include <cstdint>
-#include <optional>
 
 namespace tape {
 
 class ITape {
   public:
-    virtual std::optional<std::int32_t> read() const = 0;
+    virtual std::int32_t read() const = 0;
 
-    virtual bool write(std::int32_t value) = 0;
+    virtual void write(std::int32_t value) = 0;
 
     virtual void move_left() = 0;
 
     virtual void move_right() = 0;
 
-    virtual std::size_t size() const noexcept = 0;
+    virtual bool is_begin() const noexcept = 0;
 
-    virtual std::size_t offset() const noexcept = 0;
+    virtual bool is_end() const noexcept = 0;
 
     virtual ~ITape() = default;
 };
