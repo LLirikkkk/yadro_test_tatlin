fca75ec7ff1318107309897a25779448d7a466a7
Kirill Kotlyar <kirill.kotlyar17@gmail.com>
2026-05-12 19:59:44 +0300

    dev: main added

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,38 @@
-#include <iostream>
+#include "config-loader.h"
+#include "sort/tape-sorter.h"
+#include "tape/file-tape.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <exception>
+#include <filesystem>
+#include <format>
+#include <iostream>
+#include <string>
+
+int main(int argc, char* const* argv) {
+    if (argc != 3) {
+        std::cerr << "Arguments <input file> <output file> required" << std::endl;
+        return 1;
+    }
+
+    try {
+        if (!std::filesystem::exists("config.txt")) {
+            std::cerr << "Could not find config.txt" << std::endl;
+            return 1;
+        }
+
+        utils::ConfigLoader loader;
+        loader.read_config("config.txt");
+
+        tape::FileTape input_tape(argv[1], loader.get_tape_config());
+        tape::FileTape output_tape(argv[2], input_tape.size(), loader.get_tape_config());
+
+        tape::TapeSorter sorter(loader.get_tape_sorter_config());
+
+        sorter.sort(input_tape, output_tape);
+    } catch (const std::exception& e) {
+        std::cerr << std::format("Error: {}", e.what()) << std::endl;
+        return 1;
+    }
+
     return 0;
 }
